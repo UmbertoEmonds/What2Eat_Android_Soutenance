826c81ef0636e748e685753cb73de5f2d38cf4e9
Umberto Emonds <umberto.emonds@gmail.com>
2021-08-19 12:53:10 +0200

    modification Injection.java

--- app/src/main/java/fr/projet2/what2eat/util/injections/Injection.java
+++ app/src/main/java/fr/projet2/what2eat/util/injections/Injection.java
@@ -2,8 +2,12 @@ package fr.projet2.what2eat.util.injections;
 
 public class Injection {
 
-    public static ViewModelFactory provideViewModelFactory() {
-        return new ViewModelFactory();
-    }
+    public static ViewModelFactory VM_FACTORY;
 
+    public static ViewModelFactory provideViewModelFactory() {
+        if(VM_FACTORY == null){
+            VM_FACTORY = new ViewModelFactory();
+        }
+        return VM_FACTORY;
+    }
 }
